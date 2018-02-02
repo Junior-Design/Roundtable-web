d6254311a12dc2273cf60d19699429070530e520
Phillip Reeder <reeder.phill@gmail.com>
2018-02-02 11:02:05 -0500

    center footer buttons

--- react/components/MainLayoutFooter.jsx
+++ react/components/MainLayoutFooter.jsx
@@ -5,22 +5,31 @@ const footerStyle = {
   position: 'absolute',
   bottom: '0',
   width: '100%',
-  height: '60px'
+  height: '60px',
+  textAlign: 'center',
+  backgroundColor: '#202020'
+}
+
+const wrapperStyle = {
+  margin: 'auto'
 }
 
 const buttonStyle = {
   height: "100%",
   backgroundColor: "#202020",
-  color: "white"
+  color: "white",
+  margin: "0 10px"
 }
 
 export default class MainLayoutFooter extends React.Component {
   render() {
     return (
         <div className="mainLayoutFooter" style={footerStyle}>
+
           <Link to={`/browse`}><button style={buttonStyle}>Home</button></Link>
           <Link to={`/playlists`}><button style={buttonStyle}>Playlists</button></Link>
           <Link to={'/friends'}><button style={buttonStyle}>Friends</button></Link>
+
         </div>
     );
   }
