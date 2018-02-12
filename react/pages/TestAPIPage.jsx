58c020665a265ee539110dc531aeaaf4ee555b59
Gábor Siffel <hugabor@users.noreply.github.com>
2018-02-12 10:57:01 -0500

    renamed to comms.jsx and added cookie handlers

--- react/pages/TestAPIPage.jsx
+++ react/pages/TestAPIPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import server from '../server_requests'
+import comms from '../comms'
 
 
 export default class TestAPIPage extends React.Component {
 
   componentDidMount() {
-    server.post('/test_api_route', "posted data", function(resp) {
+    comms.post('/test_api_route', "posted data", function(resp) {
         console.log("received response:", resp)
     })
   }
