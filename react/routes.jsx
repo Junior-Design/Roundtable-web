7afa231f0dacd43175dab84a8185b1d93234f677
Gábor Siffel <hugabor@users.noreply.github.com>
2018-02-12 10:39:44 -0500

    fixed not bundling

--- react/routes.jsx
+++ react/routes.jsx
@@ -4,11 +4,7 @@ import { Route, IndexRoute } from 'react-router'
 import MainLayout from './components/MainLayout';
 
 import IndexPage from './pages/IndexPage';
-import LoginPage from './pages/LoginPage';
-import LogoutPage from './pages/LogoutPage';
 import BrowsePage from './pages/BrowsePage';
-import ConnectPage from './pages/ConnectPage';
-import ConnectSpotifyPage from './pages/ConnectSpotifyPage';
 import SpotifyConfirmationPage from './pages/SpotifyConfirmationPage';
 import PlaylistsPage from './pages/PlaylistsPage';
 import TestAPIPage from './pages/TestAPIPage';
@@ -23,11 +19,7 @@ const routes = (
       <Route path="confirm/spotify"	component={SpotifyConfirmationPage} />
       <Route path="browse"          component={BrowsePage} />
       <Route path="playlists"       component={PlaylistsPage} />
-      {/* <Route path="login"           component={LoginPage} />
-      <Route path="logout"          component={LogoutPage} />
-      <Route path="connect" exact   component={ConnectPage} />
-      <Route path="connect/spotify" component={ConnectSpotifyPage} />
-      <Route path="/testapi"        component={TestAPIPage} /> */}
+      {/* <Route path="/testapi"        component={TestAPIPage} /> */}
       <Route path="*"               component={NotFoundPage} />
     </Route>
   </Route>
