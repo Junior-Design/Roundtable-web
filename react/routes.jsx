13fef94c820b7e821acded48b3358b1075a1dacc
JakeWaldner <jakew.waldner@gmail.com>
2018-03-28 10:56:03 -0400

    added FriendsPage

--- react/routes.jsx
+++ react/routes.jsx
@@ -7,6 +7,7 @@ import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import BrowsePage from './pages/BrowsePage';
 import PlaylistsPage from './pages/PlaylistsPage';
+import FriendsPage from './pages/FriendsPage';
 import TestAPIPage from './pages/TestAPIPage';
 import NotFoundPage from './pages/NotFoundPage';
 
@@ -19,6 +20,7 @@ const routes = (
     <Route component={MainLayout}>
       <Route path="browse"          component={BrowsePage} />
       <Route path="playlists"       component={PlaylistsPage} />
+      <Route path="friends"         component={FriendsPage} />
       {/* <Route path="/testapi"        component={TestAPIPage} /> */}
       <Route path="*"               component={NotFoundPage} />
     </Route>
