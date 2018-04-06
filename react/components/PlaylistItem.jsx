281d1ae4c67879b67123b657481e51d08ba842d5
Phillip Reeder <reeder.phill@gmail.com>
2018-04-06 11:32:11 -0400

    add playlistitem add button

--- react/components/PlaylistItem.jsx
+++ react/components/PlaylistItem.jsx
@@ -4,11 +4,31 @@ import styled from 'styled-components';
 
 
 const imageStyle = {
-  width: '75px',
-  height: '75px',
+  width: '50px',
+  height: '50px',
   marginRight: '10px',
   display: 'inline-block',
   verticalAlign: 'middle'
+}
+
+const itemStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  whitespace: 'nowrap',
+  margin: '9px 0',
+  width: '100%',
+  height: '75px'
+}
+
+const buttonStyle = {
+  width: '50px',
+  height: '50px',
+  fontSize: '50px',
+  color: 'white',
+  border: 'none',
+  marginRight: '10px',
+  marginTop: '10px',
+  background: 'none'
 }
 
 class SongButton extends React.Component {
@@ -26,23 +46,29 @@ export default class PlaylistItem extends React.Component {
   constructor(props) {
     super(props)
 
-    if (props.playlist.image_url != undefined 
-      && props.playlist.image_url != "") 
-    {
-      this.state = {"image": this.props.playlist.image_url}
-    } else {
-      this.state = {"image": '/assets/images/music-placeholder.png'}
+    let name = props.playlist.name;
+    if (name.length > 23)
+      name = name.substring(0, 21) + "...";
+
+    let image = this.props.playlist.image_url;
+    if (!(props.playlist.image_url != undefined 
+      && props.playlist.image_url != "")) {
+      image = '/assets/images/music-placeholder.png';
     }
+
+    this.state = {"name":name, "image":image};
   }
   
   render() {
     return (
-  		<li className="playlistItem" onClick={this.props.onClick} style={{"margin":"9px 0", "width":"100%", "height":"75px"}}>
-        <span>
+  		<li className="playlistItem" onClick={this.props.onClick} style={itemStyle}>
+        <span style={{'marginTop':'12px'}}>
           <img src={this.state.image} style={imageStyle} />
+          <span style={{'verticalAlign':'middle', 'fontSize':"1em", 'display':'inline-block'}}>{this.state.name}</span>
         </span>
-        <span style={{"fontSize": "1em"}}>{this.props.playlist.name}</span>
-        <span>{this.props.playlist.description}</span>
+        <span style={{'verticalAlign':'middle'}}>
+          <button style={buttonStyle}>+</button>
+        </span>
       </li>
     );
   }
