ad9d62251d8a83a678a7f256363efc3da60fe22e
Phillip Reeder <reeder.phill@gmail.com>
2018-04-22 15:11:00 -0400

    fix page

--- react/pages/PlaylistPage.jsx
+++ react/pages/PlaylistPage.jsx
@@ -18,6 +18,15 @@ const rootStyle = {
   marginRight:"30px"
 }
 
+const buttonStyle = {
+  width: '50px',
+  height: '50px',
+  color: 'white',
+  border: 'none',
+  marginRight: "10px",
+  background: 'none'
+}
+
 export default class PlaylistPage extends React.Component {
   constructor(props) {
     super(props);
@@ -45,7 +54,7 @@ export default class PlaylistPage extends React.Component {
       ownButton = (<button style={buttonStyle} onClick={(e)=>this.addButtonClick()}><span style={{fontSize:"40px"}}>+</span></button>);
     if (this.state.owned && this.props.userId != null)
       ownButton = (<button style={buttonStyle}><span style={{fontSize:"30px"}}>✓</span></button>)
-      
+
     if (this.state.loading)
       ownButton = (<img src="/assets/images/spinner.gif" width="40px" height="40px"/>)
 
